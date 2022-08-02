3b106f5c88fe78a64ae40d5d5e7bbc6dd57af803
Sirshak Bohara <021neb459@sxc.edu.np>
2022-08-02 16:41:55 +0545

    added small docs

--- LinkedList/linkedlists.h
+++ LinkedList/linkedlists.h
@@ -1,10 +1,19 @@
 #pragma once
 
+/**
+ * @brief Structure of a Node.
+ * 
+ */
 struct Node
 {
     int data;
     Node* next;
 
+    /**
+     * @brief Construct a new Node object with next default to nullptr
+     * 
+     * @param value 
+     */
     Node(int value)
     {
         data = value;
@@ -18,10 +27,35 @@ class LinkedList
 public:
     LinkedList();
     ~LinkedList();
+
+public:
+    /**
+     * @brief Add data to the end of the list.
+     * 
+     * @param data The data to add.
+     */
     void Append(int data);
+
+    /**
+     * @brief Add data to the beginning of the list
+     * 
+     * @param data The data to add
+     */
     void Insert(int data);
+
+    /**
+     * @brief Add data to the given position
+     * 
+     * @param data The data to add
+     * @param position The position to add data into list
+     */
     void InsertAt(int data, int position);
-    void PrintList();
+
+    /**
+     * @brief Prints the list
+     * 
+     */
+    void PrintList(void);
 
 private:
     Node* m_headNode = nullptr;
