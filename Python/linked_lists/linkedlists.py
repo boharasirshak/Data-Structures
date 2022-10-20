62bdd1851bdabf27d85aa9eb5c8c36061049c962
Sirshak Bohara <boharasirshak101@gmail.com>
2022-10-20 20:50:33 +0545

    added deletion methods

--- Python/linked_lists/linkedlists.py
+++ Python/linked_lists/linkedlists.py
@@ -11,7 +11,7 @@ class LinkedList:
         self._head = None
 
     @property
-    def size(self):
+    def size(self) -> int:
         if self._head is None:
             return 0
 
@@ -23,7 +23,7 @@ class LinkedList:
         
         return count
         
-    def print(self):
+    def print(self) -> None:
         if self._head is None:
             print('[]')
             return 
@@ -36,10 +36,20 @@ class LinkedList:
         
         print(datas)
 
-    def push_front(self, data: Any):
+    def push_front(self, data: Any) -> None:
+        """Pushes a new element to the front of the list
+
+        Args:
+            data (Any): The data to push
+        """
         self._head = Node(data, self._head)
     
-    def push_back(self, data: Any):
+    def push_back(self, data: Any) -> None:
+        """Pushes a new element to the end of the list
+
+        Args:
+            data (Any): The data to push
+        """
         if self._head is None:
             self._head = Node(data, None)
             return
@@ -50,7 +60,12 @@ class LinkedList:
         
         tmp.next = Node(data, None)
     
-    def extend_back(self, datas: List[Any]):
+    def extend_back(self, datas: List[Any]) -> None:
+        """Extends a `list` to the back of the linked list
+
+        Args:
+            datas (List[Any]): The python `list` to extend
+        """
         tmp = self._head
         while tmp.next:
             tmp = tmp.next
@@ -59,14 +74,28 @@ class LinkedList:
             tmp.next = Node(data, None)
             tmp = tmp.next    
     
-    def extend_front(self, datas: List[Any]):
+    def extend_front(self, datas: List[Any]) -> None:
+        """Extends a `list` to the front of the linked list
+
+        Args:
+            datas (List[Any]): The python `list` to extend
+        """
         tmp = self._head
         for data in reversed(datas):
             tmp = Node(data=data, next=tmp)
         
         self._head = tmp
     
-    def push_at(self, data: Any, index: int):
+    def push_at(self, data: Any, index: int) -> None:
+        """Pushes a new element to the given `index`
+
+        Args:
+            data (Any): The element to push
+            index (int): The index to push
+
+        Raises:
+            IndexError: When the given index is more than the size of the list
+        """
         size = self.size - 1 
         if index > size:
             raise IndexError("Index more than the size of list!")
@@ -85,7 +114,18 @@ class LinkedList:
         
         tmp.next = Node(data, tmp.next)
 
-    def pop_at(self, index: int):
+    def pop_at(self, index: int) -> Node:
+        """Removes a item at the specified 
+
+        Args:
+            index (int): The index whose element is to be removed
+
+        Raises:
+            IndexError: When the given index is more than that of the size of the list
+
+        Returns:
+            Node: The Node object that was deleted
+        """
         size = self.size - 1 
         if index > size:
             raise IndexError("Index more than the size of list!")
@@ -103,19 +143,37 @@ class LinkedList:
             count += 1
         
         var = tmp.next.next
-        del tmp.next
+        elem = tmp.next
+        elem.next = None
         tmp.next = var
+        return elem
     
-    def pop(self, data: Any):
+    def pop(self, data: Any) -> Node:
+        """Removed a element which data matches with the `data`
+
+        Args:
+            data (Any): The data to match within the element
+
+        Returns:
+            Node: Returns Node object if successfully removed
+        
+        Raises:
+            ValueError: When the the element with the specified data not found
+        """
         tmp = self._head
         
         while tmp.next:
             if tmp.next.data == data:
                 var = tmp.next.next
-                del tmp.next
+                elem = tmp.next
+                elem.next = None
                 tmp.next = var
+                return elem
                 
             tmp = tmp.next
+        
+        raise ValueError("The element with the specified data not found")
+
     
 if __name__ == '__main__':
     ll = LinkedList()
