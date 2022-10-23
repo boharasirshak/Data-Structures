098cf29d5151effb339ec92f4d0f391462ff2afd
Sirshak Bohara <boharasirshak101@gmail.com>
2022-10-23 20:38:32 +0545

    completed stack  implementation in python

--- Python/stacks/stack.py
+++ Python/stacks/stack.py
@@ -1,2 +1,32 @@
+from typing import Any, List
+
 class Stack:
-    pass
+    def __init__(self, items: List[Any] = []) -> None:
+        self._stack: List[Any] = []
+        self._index = -1 # -1 stands for empty
+        for item in items:
+            self.push(item)
+    
+    @property
+    def is_empty(self) -> bool:
+        return self._index == -1
+    
+    @property
+    def top(self) -> Any:
+        if self.is_empty:
+            return None
+        return self._stack[self._index]
+    
+    def push(self, item: Any) -> None:
+        self._stack.append(item)
+        self._index += 1
+    
+    def pop(self) -> Any:
+        if self.is_empty:
+            raise IndexError("Empty stack")
+        
+        self._index -= 1
+        return self._stack.pop()
+
+if __name__ == '__main__':
+    stack = Stack()
---
+++ Python/tests/test_stacks.py
@@ -0,0 +1,25 @@
+import pytest
+from stacks import Stack
+
+def test_properties():
+    stack = Stack()    
+    assert stack.is_empty is True
+    assert stack.top is None
+    
+def test_push():
+    stack = Stack()
+    stack.push(1)
+    assert stack.top == 1
+    
+    stack = Stack([1,2,3,4])
+    assert stack.top == 4
+    
+    
+def test_pop():
+    stack = Stack()
+    with pytest.raises(IndexError) as e:
+        stack.pop()
+    assert str(e.value) == "Empty stack"
+
+    stack.push(1)
+    assert stack.pop() == 1
